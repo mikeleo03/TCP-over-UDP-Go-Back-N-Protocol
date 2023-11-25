8f3732d04d60693064da2c5a65353aa914a37686
Michael Leon Putra Widhi <leonmichael463@gmail.com>
2023-11-25 14:50:17 +0700

    feat : connection, refactor : segment

--- lib/connection.py
+++ lib/connection.py
@@ -1,19 +1,44 @@
 import socket
-from .segment import Segment
+from typing import Tuple
+from segment import Segment
+from constant import DEFAULT_BROADCAST_PORT, DEFAULT_IP, DEFAULT_PORT, SEGMENT_SIZE, TIMEOUT
 
 class Connection:
-    def __init__(self, ip : str, port : int):
+    def __init__(self, ip : str = DEFAULT_IP, broadcast_port: int = DEFAULT_BROADCAST_PORT, port: int = DEFAULT_PORT, is_server: bool = False):
         # Init UDP socket
-        pass
+        if is_server:
+            self.ip = ip
+            self.port = broadcast_port
+            self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            # Source : https://docs.python.org/3/library/socket.html
+            self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            self.socket.bind((ip, broadcast_port))
+            print(f"[!] Server started at {self.ip}:{self.port}")
+        else:
+            self.ip = ip
+            self.port = broadcast_port
+            self.client_port = port
+            self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            self.socket.bind((ip, port))
+            print(f"[!] Client started at {self.ip}:{self.port}")
 
-    def send_data(self, msg : Segment, dest : ("ip", "port")):
+    def send_data(self, msg: Segment, dest: Tuple[str, int]):
         # Send single segment into destination
-        pass
+        self.socket.sendto(msg, dest)
 
-    def listen_single_segment(self) -> Segment:
+    def listen_single_segment(self, timeout=TIMEOUT) -> Segment:
         # Listen single UDP datagram within timeout and convert into segment
-        pass
+        try:
+            self.socket.settimeout(timeout)
+            return self.socket.recvfrom(SEGMENT_SIZE)
+        except TimeoutError as e:
+            raise e
 
     def close_socket(self):
         # Release UDP socket
-        pass
+        self.socket.close()
+        
+    def __str__(self):
+        # String overriding for easier print(connection)
+        print(f"ip: {self.ip}\n broadcast_port: {self.port}\n  client port:{self.client_port}\n")
+        
--- lib/segment.py
+++ lib/segment.py
@@ -22,47 +22,47 @@ class Segment:
     # -- Internal Function --
     def __init__(self):
         # Initalize segment
-        self.seqNum = 0
-        self.ackNum = 0
+        self.seq_num = 0
+        self.ack_num = 0
         self.flag = SegmentFlag(0b0)   # Default to 0
         self.checksum = 0
-        self.data = b""  # Binary data
+        self.payload = b""  # Binary payload
 
     def __str__(self):
         # Optional, override this method for easier print(segmentA)
         output = ""
-        output += f"{'SeqNum':12}\t\t| {self.seqNum}\n"
-        output += f"{'AckNum':12}\t\t| {self.ackNum}\n"
+        output += f"{'SeqNum':12}\t\t| {self.seq_num}\n"
+        output += f"{'AckNum':12}\t\t| {self.ack_num}\n"
         output += f"{'FlagSYN':12}\t\t| {self.flag.syn >> 1}\n"
         output += f"{'FlagACK':12}\t\t| {self.flag.ack >> 4}\n"
         output += f"{'FlagFIN':12}\t\t| {self.flag.fin}\n"
         output += f"{'Checksum':24}| {self.checksum}\n"
-        output += f"{'MsgSize':24}| {len(self.data)}\n"
+        output += f"{'MsgSize':24}| {len(self.payload)}\n"
         return output
 
     def __calculate_checksum(self) -> int:
         # Calculate checksum here, return checksum result
         # TODO : implement checksum
-        pass
+        return 10
 
 
     # -- Setter --
     def set_header(self, header : dict):
-        self.seqNum = header["seq"]
-        self.ackNum = header["ack"]
+        self.seq_num = header["seq_num"]
+        self.ack_num = header["ack_num"]
 
     def set_payload(self, payload : bytes):
-        self.data = payload
+        self.payload = payload
 
     def set_flag(self, flag_list : list):
         new_flag = 0b0
         for flag in flag_list:
             if flag == "SYN":
-                new_flag |= SYN_FLAG
+                new_flag = new_flag | SYN_FLAG
             elif flag == "ACK":
-                new_flag |= ACK_FLAG
+                new_flag = new_flag | ACK_FLAG
             elif flag == "FIN":
-                new_flag |= FIN_FLAG
+                new_flag = new_flag | FIN_FLAG
         self.flag = SegmentFlag(new_flag)
 
 
@@ -71,32 +71,32 @@ class Segment:
         return self.flag.get_flag()
 
     def get_header(self) -> dict:
-        return {"seq": self.seq, "ack": self.ack}
+        return {"seq_num": self.seq_num, "ack_num": self.ack_num}
 
     def get_payload(self) -> bytes:
-        return self.data
+        return self.payload
 
 
     # -- Marshalling --
     def set_from_bytes(self, src : bytes):
         # From pure bytes, unpack() and set into python variable
-        self.seqNum = struct.unpack("I", src[0:4])[0]               # Byte 0-3 (unsigned int [4])
-        self.ackNum = struct.unpack("I", src[4:8])[0]               # Byte 4-7 (unsigned int [4])
-        self.flag = SegmentFlag(struct.unpack("B", src[8:9])[0])    # Byte 8 (unsigned char [1])
+        self.seq_num = struct.unpack("I", src[0:4])[0]               # Byte 0-3 (unsigned int [4])
+        self.ack_num = struct.unpack("I", src[4:8])[0]               # Byte 4-7 (unsigned int [4])
+        self.flag = SegmentFlag(struct.unpack("B", src[8:9])[0])     # Byte 8 (unsigned char [1])
         # Byte 9 empty -> padding
-        self.checksum = struct.unpack("H", src[10:12])[0]           # Byte 10-11 (unsigned short [2])
-        self.data = src[12:]                                        # Byte 12-end -> payload
+        self.checksum = struct.unpack("H", src[10:12])[0]            # Byte 10-11 (unsigned short [2])
+        self.payload = src[12:]                                      # Byte 12-end -> payload
 
     def get_bytes(self) -> bytes:
         # Convert this object to pure bytes
         self.checksum = self.__calculate_checksum()
         res = b""
-        res += struct.pack("I", self.seqNum)        # Byte 0-3 (unsigned int [4])
-        res += struct.pack("I", self.ackNum)        # Byte 4-7 (unsigned int [4])
-        res += self.flag.get_flag_bytes()           # Byte 8 (unsigned char [1])
-        res += struct.pack("x")                     # Byte 9 -> padding
-        res += struct.pack("H", self.checksum)      # Byte 10-11 (unsigned short [2])
-        res += self.data                            # Byte 12-end -> payload
+        res += struct.pack("I", self.seq_num)        # Byte 0-3 (unsigned int [4])
+        res += struct.pack("I", self.ack_num)        # Byte 4-7 (unsigned int [4])
+        res += self.flag.get_flag_bytes()            # Byte 8 (unsigned char [1])
+        res += struct.pack("x")                      # Byte 9 -> padding
+        res += struct.pack("H", self.checksum)       # Byte 10-11 (unsigned short [2])
+        res += self.payload                          # Byte 12-end -> payload
         return res
 
 
