b7972172d20bc06ee6eea0eadaf47795f8282d80
GoDillonAudris512 <110383663+GoDillonAudris512@users.noreply.github.com>
2023-11-26 01:09:27 +0700

    chore: add logging

--- lib/connection.py
+++ lib/connection.py
@@ -1,10 +1,15 @@
 import socket
+import logging
+import colorlog
 from typing import Tuple
 from .segment import Segment
 from .constant import DEFAULT_BROADCAST_PORT, DEFAULT_IP, DEFAULT_PORT, SEGMENT_SIZE, TIMEOUT
 
 class Connection:
     def __init__(self, ip : str = DEFAULT_IP, broadcast_port : int = DEFAULT_BROADCAST_PORT, port : int = DEFAULT_PORT, is_server : bool = False):
+        # Logger
+        self.logger = self.setup_logger()
+
         # Init UDP socket
         if is_server:
             self.ip = ip
@@ -13,15 +18,40 @@ class Connection:
             # Source : https://docs.python.org/3/library/socket.html
             self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
             self.socket.bind((ip, broadcast_port))
-            print(f"[!] Server started at {self.ip}:{self.port}")
+            self.logger.info(f"[!] Server started at {self.ip}:{self.port}")
         else:
             self.ip = ip
             self.port = broadcast_port
             self.client_port = port
             self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
             self.socket.bind((ip, port))
-            print(f"[!] Client started at {self.ip}:{self.port}")
+            self.logger.info(f"[!] Client started at {self.ip}:{self.port}")
 
+    def setup_logger(self):
+        # Set up logging configuration
+        logger = logging.getLogger(__name__)
+        logger.setLevel(logging.DEBUG)
+
+        # Create console handler
+        ch = logging.StreamHandler()
+        ch.setLevel(logging.DEBUG)
+
+        # Create formatter
+        formatter = colorlog.ColoredFormatter("%(asctime)s - %(log_color)s%(message)s", 
+                                              log_colors={'DEBUG': 'white',
+                                                          'INFO': 'green',
+                                                          'WARNING': 'yellow',
+                                                          'ERROR': 'red',
+                                                          'CRITICAL': 'bold_red',
+                                              })
+
+        # Add formatter to ch
+        ch.setFormatter(formatter)
+
+        # Add ch to logger
+        logger.addHandler(ch)
+        return logger
+    
     def send_data(self, msg: Segment, dest: Tuple[str, int]):
         # Send single segment into destination
         self.socket.sendto(msg, dest)
