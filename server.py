ed39f4bb3bc8451026543c4c529c65ee08670b2a
GoDillonAudris512 <110383663+GoDillonAudris512@users.noreply.github.com>
2023-11-26 15:03:48 +0700

    feat: implement server paralelization process

--- server.py
+++ server.py
@@ -1,9 +1,12 @@
 import os
 import logging
 import colorlog
+import threading
+import time
+
 from math import ceil
 from socket import timeout
-from typing import List, Tuple
+from typing import List, Tuple, Dict
 
 from lib.connection import Connection
 from lib.segment import Segment
@@ -11,7 +14,11 @@ from lib.argparse import FileTransferArgumentParser as Parser
 from lib.constant import SYN_FLAG, ACK_FLAG, FIN_ACK_FLAG, PAYLOAD_SIZE, SYN_ACK_FLAG, TIMEOUT_LISTEN, WINDOW_SIZE
 
 class Server:
+    # -- Constructor --
     def __init__(self):
+        # Logger
+        self.logger = self.setup_logger()
+        
         # Init server
         args = Parser(is_server=True)
         server_arguments = args.get_value()
@@ -26,8 +33,7 @@ class Server:
         self.filename = self.get_filename()
         self.breakdown_file()
 
-        # Logger
-        self.logger = self.setup_logger()
+        self.parallel = False
 
         self.logger.debug(f"[!] Source file | {self.filename} | {self.filesize} bytes")
 
@@ -56,6 +62,8 @@ class Server:
         logger.addHandler(ch)
         return logger
     
+
+    # -- Input File Handler --
     def open_file(self):
         try:
             file = open(f"{self.pathfile}", "rb")
@@ -112,32 +120,65 @@ class Server:
         self.file.seek(offset)
         return self.file.read(PAYLOAD_SIZE)
     
+
+    # -- Listening Handler --
+    def ask_for_parallel(self):
+        choice = input("[?] Enable paralelization for server (y/n) ").lower()
+        while not self.choice_valid(choice):
+            print("[!] Please input correct input")
+            choice = input("[?] Enable paralelization for server (y/n) ").lower()
+
+        if choice == "y":
+            self.parallel = True
+
     def listen_for_clients(self):
         self.logger.debug("[!] Listening to broadcast address for clients.")
         while True:
-            try:
-                client = self.connection.listen_single_segment(TIMEOUT_LISTEN)
-                client_address = client[1]
-                ip, port = client_address
-                self.client_list.append(client_address)
-                self.logger.debug(f"[!] Received request from {ip}:{port}")
-                choice = input("[?] Listen more (y/n) ").lower()
-                while not self.choice_valid(choice):
-                    print("[!] Please input correct input")
+            if self.parallel:
+                self.parallel_listen()
+            else:
+                try:
+                    client = self.connection.listen_single_segment(TIMEOUT_LISTEN)
+                    client_address = client[1]
+                    ip, port = client_address
+                    self.client_list.append(client_address)
+                    self.logger.debug(f"[!] Received request from {ip}:{port}")
                     choice = input("[?] Listen more (y/n) ").lower()
-                if choice == "n":
-                    print("\nClient list:")
-                    for index, (ip, port) in enumerate(self.client_list):
-                        print(f"{index+1}. {ip}:{port}")
-                    print("")
+                    while not self.choice_valid(choice):
+                        print("[!] Please input correct input")
+                        choice = input("[?] Listen more (y/n) ").lower()
+                    if choice == "n":
+                        print("\nClient list:")
+                        for index, (ip, port) in enumerate(self.client_list):
+                            print(f"{index+1}. {ip}:{port}")
+                        print("")
+                        break
+                except timeout:
+                    if (len(self.client_list) == 0):
+                        self.logger.error("[!] Timeout error for listening client. Exiting")
+                    else:
+                        self.logger.warning("[!] Timeout error for listening client")
                     break
-            except timeout:
-                if (len(self.client_list) == 0):
-                    self.logger.error("[!] Timeout error for listening client. Exiting")
+
+    def parallel_listen(self):
+        self.parallel_client_list: Dict[Tuple[str, int], List[Segment]] = {}
+        while True:
+            try:
+                client = self.connection.listen_single_segment(15)
+                client_address = client[1]
+                if client_address not in self.parallel_client_list:
+                    ip, port = client_address 
+                    self.logger.debug(f"[!] Received request from {ip}:{port}")
+
+                    self.parallel_client_list[client_address] = []
+                    new_thread = threading.Thread(target=self.start_file_transfer, kwargs={"parallel_client": client_address})
+                    new_thread.start()
                 else:
-                    self.logger.warning("[!] Timeout error for listening client")
-                break
-            
+                    self.parallel_client_list[client_address].append(client[0])
+            except timeout:
+                self.logger.error("[!] Timeout error for listening client. Exiting")
+                exit(0)
+
     def choice_valid(self, choice: str):
         if choice.lower() == "y":
             return "y"
@@ -146,12 +187,29 @@ class Server:
         else:
             return False
         
-    def start_file_transfer(self):
-        self.logger.debug("[!] Commencing file transfer...")
-        for client in self.client_list:
-            self.three_way_handshake(client)
-            self.file_transfer(client)
 
+    # -- File Transfer Handler --
+    def start_file_transfer(self, parallel_client=None):
+        self.logger.debug("[!] Commencing file transfer...")
+        
+        if self.parallel:
+            self.three_way_handshake(parallel_client)
+            self.file_transfer(parallel_client)
+        else:
+            for client in self.client_list:
+                self.three_way_handshake(client)
+                self.file_transfer(client)
+
+    def get_segment(self, client_address: Tuple[str, int]):
+        if self.parallel:
+            just_now = time.time() + 1
+            while (time.time() < just_now):
+                if len(self.parallel_client_list[client_address]) > 0:
+                    return self.parallel_client_list[client_address].pop(0), client_address
+            raise timeout
+        else:
+            return self.connection.listen_single_segment()
+        
     def three_way_handshake(self, client_address: Tuple[str, int]) -> bool:
         # Three Way Handshake Protocol, for server-side to establishing connection with client
         self.logger.debug(f"[!] [Client {client_address[0]}:{client_address[1]}] Initiating three way handshake")
@@ -175,7 +233,7 @@ class Server:
                 
                 # Wait for SYN-ACK from client
                 try:
-                    data, client_address = self.connection.listen_single_segment()
+                    data, client_address = self.get_segment(client_address)
                     self.segment.set_from_bytes(data)
                 except timeout:
                     self.logger.error(f"[!] [Client {client_address[0]}:{client_address[1]}] SYN-ACK response timeout. Resending SYN")
@@ -203,7 +261,7 @@ class Server:
         
         self.logger.info(f"[!] [Client {client_address[0]}:{client_address[1]}] Handshake established")
     
-    def file_transfer(self, client_addr: Tuple[str, int]):
+    def file_transfer(self, client_address: Tuple[str, int]):
         # File transfer, server-side
         # seq_num 0 for SYN
         # seq_num 1 for ACK
@@ -217,71 +275,75 @@ class Server:
             
             for i in range(sequence_max):
                 # Start sending segment x
-                self.logger.debug(f"[!] [Client {client_addr[0]}:{client_addr[1]}] Sending Segment {sequence_base + i}")
+                self.logger.debug(f"[!] [Client {client_address[0]}:{client_address[1]}] Sending Segment {sequence_base + i}")
                 if i + sequence_base < num_of_segment:
-                    self.connection.send_data(self.list_segment[i + sequence_base - 2].get_bytes(), client_addr)
+                    self.connection.send_data(self.list_segment[i + sequence_base - 2].get_bytes(), client_address)
                     
             for i in range(sequence_max):
                 try:
-                    data, response_addr = self.connection.listen_single_segment()
+                    data, response_address = self.get_segment(client_address)
                     segment = Segment()
                     segment.set_from_bytes(data)
 
                     # Various segment conditions
-                    if (client_addr[1] == response_addr[1] and segment.get_flag() == ACK_FLAG and segment.get_header()["ack_num"] == sequence_base + 1):
-                        self.logger.debug(f"[!] [Client {client_addr[0]}:{client_addr[1]}] Received ACK {sequence_base + 1}")
+                    if (client_address[1] == response_address[1] and segment.get_flag() == ACK_FLAG and segment.get_header()["ack_num"] == sequence_base + 1):
+                        self.logger.debug(f"[!] [Client {client_address[0]}:{client_address[1]}] Received ACK {sequence_base + 1}")
                         sequence_base += 1
                         window_size = min(num_of_segment - sequence_base, WINDOW_SIZE)
-                    elif client_addr[1] != response_addr[1]:
-                        self.logger.warning(f"[!] [Client {client_addr[0]}:{client_addr[1]}] Received ACK from wrong client")
+                    elif client_address[1] != response_address[1]:
+                        self.logger.warning(f"[!] [Client {client_address[0]}:{client_address[1]}] Received ACK from wrong client")
                     elif segment.get_flag() == SYN_ACK_FLAG:
-                        self.logger.debug(f"[!] [Client {client_addr[0]}:{client_addr[1]}] Received SYN ACK Flag, client ask to reset connection")
+                        self.logger.debug(f"[!] [Client {client_address[0]}:{client_address[1]}] Received SYN ACK Flag, client ask to reset connection")
                         reset_conn = True
                         break
                     elif segment.get_flag() != ACK_FLAG:
-                        self.logger.warning(f"[!] [Client {client_addr[0]}:{client_addr[1]}] Received Wrong Flag")
+                        self.logger.warning(f"[!] [Client {client_address[0]}:{client_address[1]}] Received Wrong Flag")
                     else:
-                        self.logger.warning(f"[!] [Client {client_addr[0]}:{client_addr[1]}] Received Wrong ACK")
+                        self.logger.warning(f"[!] [Client {client_address[0]}:{client_address[1]}] Received Wrong ACK")
                         request_number = segment.get_header()["ack_num"]
                         if (request_number > sequence_base):
                             sequence_max = (sequence_max - sequence_base) + request_number
                             sequence_base = request_number
 
                 except timeout:
-                    self.logger.error(f"[!] [Client {client_addr[0]}:{client_addr[1]}] ACK response timeout. Resending previous sequence number")
+                    self.logger.error(f"[!] [Client {client_address[0]}:{client_address[1]}] ACK response timeout. Resending previous sequence number")
         
         if reset_conn:
-            self.three_way_handshake(client_addr)
-            self.file_transfer(client_addr)
+            self.three_way_handshake(client_address)
+            self.file_transfer(client_address)
         else:
-            self.logger.info(f"[!] [Client {client_addr[0]}:{client_addr[1]}] File transfer complete. Sending FIN")
+            self.logger.info(f"[!] [Client {client_address[0]}:{client_address[1]}] File transfer complete. Sending FIN")
             sendFIN = Segment()
             sendFIN.set_flag(["FIN"])
-            self.connection.send_data(sendFIN.get_bytes(), client_addr)
+            self.connection.send_data(sendFIN.get_bytes(), client_address)
             is_ack = False
 
             # Wait for ack
             while not is_ack:
                 try:
-                    data, response_addr = self.connection.listen_single_segment()
+                    data, response_address = self.get_segment(client_address)
                     segment = Segment()
                     segment.set_from_bytes(data)
-                    if (client_addr[1] == response_addr[1] and segment.get_flag() == FIN_ACK_FLAG):
-                        self.logger.debug(f"[!] [Client {client_addr[0]}:{client_addr[1]}] Received FIN-ACK")
+                    if (client_address[1] == response_address[1] and segment.get_flag() == FIN_ACK_FLAG):
+                        self.logger.debug(f"[!] [Client {client_address[0]}:{client_address[1]}] Received FIN-ACK")
                         sequence_base += 1
                         is_ack = True
+                        if self.parallel:
+                            self.parallel_client_list.pop(client_address)
                         
                 except timeout:
-                    self.logger.error(f"[!] [Client {client_addr[0]}:{client_addr[1]}] ACK response timeout. Resending FIN")
-                    self.connection.send_data(sendFIN.get_bytes(), client_addr)
+                    self.logger.error(f"[!] [Client {client_address[0]}:{client_address[1]}] ACK response timeout. Resending FIN")
+                    self.connection.send_data(sendFIN.get_bytes(), client_address)
 
             # send ACK and tear down connection
-            self.logger.info(f"[!] [Client {client_addr[0]}:{client_addr[1]}] Sending ACK. Tearing down connection.")
+            self.logger.info(f"[!] [Client {client_address[0]}:{client_address[1]}] Sending ACK. Tearing down connection.")
             segmentACK = Segment()
             segmentACK.set_flag(["ACK"])
-            self.connection.send_data(segmentACK.get_bytes(), client_addr)
+            self.connection.send_data(segmentACK.get_bytes(), client_address)
 
 if __name__ == '__main__':
     main = Server()
+    main.ask_for_parallel()
     main.listen_for_clients()
-    main.start_file_transfer()
+    if not main.parallel:
+        main.start_file_transfer()
